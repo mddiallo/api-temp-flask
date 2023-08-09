bcd8402d5de0247345ee64eb922c7f37f6569236
Mouhamadou DIALLO <mouhamadou.diallo@hotmail.com>
2023-08-09 14:05:03 +0000

    upd3

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+@app.route('/convert', methods=['POST'])
+def convert():
+    data = request.get_json()
+    celsius = data.get('celsius')
+    if celsius is None:
+        return jsonify({'error': 'Missing "celsius" value'}), 400
+    fahrenheit = (celsius * 9/5) + 32
+    return jsonify({'celsius': celsius, 'fahrenheit': fahrenheit})
+
+if __name__ == '__main__':
+    app.run(debug=True)
